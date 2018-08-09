bbfbbd9852e3dec22f4b3ea5d9fcb89f0b614f10
BogdanP <4netit2@gmail.com>
2018-08-09 09:40:35 +0300

    Test if it is an URL and add some comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -14,26 +14,27 @@ $(function() {
     * feeds definitions, the allFeeds variable in our application.
     */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+        /* This it tests to make sure that the
+         * allFeeds variable has been defined and that it is not empty.
          */
         it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
+            expect(allFeeds).toBeDefined();          //check if allFeeds array is defined
+            expect(allFeeds.length).not.toBe(0);     //check if contains something
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* This is a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it ('test if has a URL', function() {
+            for (var check of allFeeds) {
+                expect(check.url).toBeDefined();        //for each feed we expect that url to be defined
+                expect(check.url.length).not.toBe(0);   //check that url is not empty
+            }
+        });
 
-
-        /* TODO: Write a test that loops through each feed
+        /* This is a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -48,21 +49,21 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-       
+
         describe('menu', function() {
             it('hidden menu', function () {
                 expect($('body').hasClass('menu-hidden')).toBe(true);
             });
-        
-       
-           
+
+
+
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-        
-          
+
+
  it('changes visibility when menu icon is clicked', function() {
     $('.menu-icon-link').click();
         expect($('body').hasClass('menu-hidden')).toBe(false);
@@ -108,5 +109,7 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-    });  
+
+      
+    }); 
 }());
